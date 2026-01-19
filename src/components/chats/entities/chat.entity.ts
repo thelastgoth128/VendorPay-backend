0dcb3080da9a2624e108134215f624df2b97ac7a
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:13:58 +0200

    feat: add columns to chat table

--- src/components/chats/entities/chat.entity.ts
+++ src/components/chats/entities/chat.entity.ts
@@ -1,1 +1,18 @@
-export class Chat {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Chat {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    chat_type: string;
+
+    @Column()
+    created_by: string;
+
+    @Column({
+        type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP"
+    })
+    created_at: Date
+}
