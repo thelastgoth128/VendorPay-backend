20aac8fa18e5e570af13ea9503ca0dc7c4f9a8c8
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:16:44 +0200

    feat: add entity decorator from type orm

--- src/components/chats/entities/chat.entity.ts
+++ src/components/chats/entities/chat.entity.ts
@@ -1,5 +1,6 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity("Chat")
 export class Chat {
     @PrimaryGeneratedColumn()
     id: string;
