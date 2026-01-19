7d17dab35728dfc737b9b7591f3230e9fc5c0a9b
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:08:53 +0200

    feat: add table columns

--- src/components/users/entities/user.entity.ts
+++ src/components/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('User')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({unique: true})
+    phone_number: string;
+
+    @Column()
+    role: string;
+
+    @Column ({
+        type: 'timestamp', default:()=>'CURRENT_TIMESTAMP'
+    })
+    created_at : Date
+
+    @Column({
+        type: "timestamp",
+    })
+    updated_at: Date;
+}
