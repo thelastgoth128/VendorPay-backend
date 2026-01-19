6911eb19a08db663de9ec5db34864fa9a7bd9b8f
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:23:22 +0200

    feat: add columns in chat participants table

--- src/components/chat_participants/entities/chat_participant.entity.ts
+++ src/components/chat_participants/entities/chat_participant.entity.ts
@@ -1,1 +1,18 @@
-export class ChatParticipant {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Chat_participant")
+export class ChatParticipant {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()//(FK -> chat.id)
+    chat_id: string;
+
+    @Column()//(FK -> user.id)
+    user_id: string;
+
+    @Column({ type: "timestamp",
+        default: () => 'CURRENT_TIMESTAMP'
+     })
+    joined_at: Date;
+}
