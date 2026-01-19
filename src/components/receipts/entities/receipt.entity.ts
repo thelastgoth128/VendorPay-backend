6b8d74c99b514e487ca211b174d95a7165995553
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:35:00 +0200

    feat: add columns to receipt table

--- src/components/receipts/entities/receipt.entity.ts
+++ src/components/receipts/entities/receipt.entity.ts
@@ -1,1 +1,19 @@
-export class Receipt {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Receipt")
+export class Receipt {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    transaction_id: string;
+
+    @Column({unique: true})
+    receipt_number: string;
+
+    @Column({type: "timestamp"})
+    issued_at: Date;
+
+    @Column()
+    metadata: string;
+}
