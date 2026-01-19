22c489d2d88a8e7a9e3cddcea93e52a02165ff57
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:31:42 +0200

    feat: add columns to the transaction table

--- src/components/transactions/entities/transaction.entity.ts
+++ src/components/transactions/entities/transaction.entity.ts
@@ -1,1 +1,37 @@
-export class Transaction {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Transaction")
+export class Transaction {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    chat_id: string;
+
+    @Column()
+    sender_id: string;
+
+    @Column()
+    receiver_id: string;
+
+    @Column()
+    amount: number;
+
+    @Column({default: "MWK"})
+    currency: string;
+
+    @Column()
+    status: string;
+
+    @Column()
+    payment_provider: string;
+
+    @Column()
+    reference_id: string;
+
+    @Column({type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP"
+     })
+    created_at: Date
+    
+}
