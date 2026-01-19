4ae8f7ecfde3892b7ce7631198b149b32edbf480
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-19 21:26:49 +0200

    feat: add columns to message tables

--- src/components/messages/entities/message.entity.ts
+++ src/components/messages/entities/message.entity.ts
@@ -1,1 +1,24 @@
-export class Message {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("Message")
+export class Message {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    chat_id: string; //(FK -> chat.id)
+
+    @Column()
+    sender_id: string; //(FK -> user.id)
+
+    @Column()
+    message_type: string; // to be an enum(text,transaction,receipt)
+
+    @Column()
+    content: string;
+
+    @Column({ type: "timestamp",
+        default: () => "CURRENT_TIMESTAMP"
+     })
+     created_at: Date;
+}
