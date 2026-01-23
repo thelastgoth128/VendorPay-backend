ba103f1f878202e552c0b6c199063ec540cad7db
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-23 23:00:12 +0200

    feat: implement AUTH validation pipes

--- src/components/auth/dto/create-auth.dto.ts
+++ src/components/auth/dto/create-auth.dto.ts
@@ -1,1 +1,9 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateAuthDto {
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    password: string;
+}
