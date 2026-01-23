de2dcacb3bf243dd72134995356902a2c20e9199
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-23 23:04:27 +0200

    feat: export and import service and entity

--- src/components/chat_participants/chat_participants.module.ts
+++ src/components/chat_participants/chat_participants.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatParticipantsService } from './chat_participants.service';
 import { ChatParticipantsController } from './chat_participants.controller';
+import { ChatParticipant } from './entities/chat_participant.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ChatParticipant])],
   controllers: [ChatParticipantsController],
   providers: [ChatParticipantsService],
+  exports: [ChatParticipantsService],
 })
-export class ChatParticipantsModule {}
+export class ChatParticipantsModule { }
