ec19dddd3dcb38bba1989eabd060800f39e7b23e
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-23 22:55:09 +0200

    feat: DB implementations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './components/users/users.module';
@@ -10,8 +12,31 @@ import { ReceiptsModule } from './components/receipts/receipts.module';
 import { AuthModule } from './components/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ChatsModule, ChatParticipantsModule, MessagesModule, TransactionsModule, ReceiptsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    ChatsModule,
+    ChatParticipantsModule,
+    MessagesModule,
+    TransactionsModule,
+    ReceiptsModule,
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
