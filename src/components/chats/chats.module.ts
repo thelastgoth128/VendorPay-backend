ad10e4094784232484a695cbb26f31c16624b867
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-24 22:10:50 +0200

    chore: add imports and exports

--- src/components/chats/chats.module.ts
+++ src/components/chats/chats.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatsService } from './chats.service';
 import { ChatsController } from './chats.controller';
+import { Chat } from './entities/chat.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chat])],
   controllers: [ChatsController],
   providers: [ChatsService],
+  exports: [ChatsService],
 })
-export class ChatsModule {}
+export class ChatsModule { }
