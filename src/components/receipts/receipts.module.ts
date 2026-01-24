683b6f2db473803a65ab673b200fde47b58a2e53
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-24 22:11:25 +0200

    chore: add imports and exports

--- src/components/receipts/receipts.module.ts
+++ src/components/receipts/receipts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReceiptsService } from './receipts.service';
 import { ReceiptsController } from './receipts.controller';
+import { Receipt } from './entities/receipt.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Receipt])],
   controllers: [ReceiptsController],
   providers: [ReceiptsService],
+  exports: [ReceiptsService],
 })
-export class ReceiptsModule {}
+export class ReceiptsModule { }
