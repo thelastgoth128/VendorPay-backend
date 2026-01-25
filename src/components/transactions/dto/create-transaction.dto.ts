ac887e7255c603b4e7d150cd93f8b46b2843173a
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-25 23:45:30 +0200

    feat: implement a transactio dto

--- src/components/transactions/dto/create-transaction.dto.ts
+++ src/components/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateTransactionDto {
+    @IsNotEmpty()
+    @IsNumber()
+    amount: number;
+
+    @IsNotEmpty()
+    @IsString()
+    currency: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    senderId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    receiverId: number;
+}
--- src/components/transactions/dto/update-transaction.dto.ts
+++ src/components/transactions/dto/update-transaction.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTransactionDto } from './create-transaction.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {}
+export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {
+    @IsOptional()
+    @IsString()
+    status?: string;
+}
