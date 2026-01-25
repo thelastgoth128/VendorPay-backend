dbdb60244673680540fd5432a7074bfcf473c6fc
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-25 23:47:26 +0200

    chore: add white spaces

--- src/components/messages/dto/create-message.dto.ts
+++ src/components/messages/dto/create-message.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMessageDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateMessageDto {
+    @IsNotEmpty()
+    @IsString()
+    content: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    senderId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    chatId: number;
+}
